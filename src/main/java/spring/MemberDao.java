641b651d85e3ad58f5b2670488d0edd5f0449982
yebbii <ogoo0133@gmail.com>
2022-12-28 14:17:13 +0900

    MemberInfoPrinter 클래스를 스프링 설정 클래스(AppCtx)에 등록

--- src/main/java/spring/MemberDao.java
+++ src/main/java/spring/MemberDao.java
@@ -1,17 +1,37 @@
 package spring;
 
-import java.lang.reflect.Member;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MemberDao {
-
+	// 멤버의 ID를 관리하는 필드
+	// 멤버 생성 시 마다 하나씩 증가시켜 중복되지 않도록 관리
+	private static long nextId = 0;
+	
+	// 멤버 정보(객체)를 저장하는 맵 객체를 정의
+	// 멤버 정보의 이메일을 키로 사용
+	private Map<String, Member> map = new HashMap<>();
+	
+	// 이메일을 이용해서 멤버를 조회해서 반환 
 	public Member selectByEmail(String email) {
-		// TODO Auto-generated method stub
-		return null;
+		return map.get(email);
 	}
-
-	public void insert(Memeber newMember) {
-		// TODO Auto-generated method stub
-		
+	
+	// 멤버 정보를 맵에 추가
+	public void insert(Member member) {
+		nextId ++;
+		member.setId(nextId);
+		map.put(member.getEmail(), member);
 	}
-
+	
+	// 멤버 정보를 맵에 업데이트
+	public void update(Member member) {
+		map.put(member.getEmail(), member);
+	}
+	
+	// 모든 멤버의 정보를 반환
+	public Collection<Member> selectAll() {
+		return map.values();
+	}
 }
--- src/main/java/spring/MemberInfoPrinter.java
+++ src/main/java/spring/MemberInfoPrinter.java
@@ -1,20 +1,28 @@
 package spring;
 
+//회원 정보를 조회해서 	=> MemberDao
+//출력하는 기능을 구현 	=> MemberPrinter
 public class MemberInfoPrinter {
 
-	public void printMemberInfo(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	// 의존 객체를 필드로 추가
+	private MemberDao memberDao;
+	private MemberPrinter printer;
+	
+	// setter 메서드를 통해서 의존 객체를 주입
 	public void setMemberDao(MemberDao memberDao) {
-		// TODO Auto-generated method stub
-		
+		this.memberDao = memberDao;
 	}
-
-	public void setMemberPrinter(MemberPrinter memberPrinter) {
-		// TODO Auto-generated method stub
-		
+	public void setMemberPrinter(MemberPrinter printer) {
+		this.printer = printer;
 	}
-
+	
+	// 이메일과 일치하는 회원의 정보를 출력하는 메서드 
+	public void printMemberInfo(String email) {
+		Member member = memberDao.selectByEmail(email);
+		if (member == null) {
+			System.out.println("일치하는 회원 정보가 없습니다.");
+			return;
+		}
+		printer.print(member);		
+	}	
 }
